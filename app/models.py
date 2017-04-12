c0fe5397a84b34f8c647e2f7e67b33e796fba4ec
Daniega <daniel.gaishuber@gmail.com>
2017-04-12 14:39:03 +0300

    Issues 4,5,6 Done

--- app/models.py
+++ app/models.py
@@ -6,11 +6,13 @@ class User(db.Model):
     first_name = db.Column(db.String(120), index=True, unique=False)
     last_name = db.Column(db.String(120), index=True, unique=False)
     id_number = db.Column(db.String(9), index=True, unique=False)
+    voted = db.Column(db.Boolean, unique = False, default=False)
 
-    def __init__(self, first_name, last_name, id_number):
+    def __init__(self, first_name, last_name, id_number, voted=False):
         self.first_name = first_name
         self.last_name = last_name
         self.id_number = id_number
+        self.voted = voted
 
     @property
     def is_authenticated(self):
@@ -23,6 +25,10 @@ class User(db.Model):
     @property
     def is_anonymous(self):
         return False
+
+    @property
+    def user_voted(self):
+        return True
 
     def get_id(self):
         try:
@@ -38,10 +44,12 @@ class Party(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), index=True, unique=False)
     picture = db.Column(db.String(120), index=True, unique=False)
+    vote_count = db.Column(db.Integer, index=True, unique=False)
 
-    def __init__(self, name, picture):
+    def __init__(self, name, picture, vote_count = 0):
         self.name = name
         self.picture = picture
+        self.vote_count = vote_count
 
     def get_id(self):
         try:
--- app/views.py
+++ app/views.py
@@ -9,7 +9,7 @@ from flask_login import login_user, logout_user, current_user, login_required
 from app import app, login_manager
 from .forms import LoginForm
 from .models import User, Party
-
+from app import db
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -17,21 +17,26 @@ def load_user(user_id):
 
 
 def validateAndAdd(party_name):
-    ## implement me!
-    pass
+    cur_user = User.query.filter_by(id_number=current_user.id_number).all()
+    party = Party.query.filter_by(name=party_name).first()
+    party.vote_count += 1
+    for user in cur_user:
+        user.voted = True
+    db.session.commit()
+
 
 
 @app.route('/', methods=['GET'])
 @app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
+    error = None
     if request.method == 'POST':
         validateAndAdd(request.form['party_name'])
         return redirect(url_for('login'))
     g.user = current_user #global user parameter used by flask framwork
     parties = Party.query.all()
-    return render_template('index.html', title='Home', user=g.user,
-                           parties=parties)
+    return render_template('index.html', title='Home', user=g.user, parties=parties, error=error)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -42,15 +47,21 @@ def login():
         first_name = request.form['first_name']
         last_name = request.form['last_name']
         id_number = request.form['id_number']
-        check_db = User.query.filter_by(id_number=id_number).first()
+        check_db = User.query.filter_by(id_number=id_number).all()
         if check_db == None:
             error = u'המצביע אינו מופיע בבסיס הנתונים'
-        elif first_name == check_db.first_name and last_name == check_db.last_name and id_number == check_db.id_number:
-           # user = User.query.filter_by(id_number=id_number).first()
-            login_user(check_db)  ## built in 'flask login' method that creates a user session
-            return redirect(url_for('index'))
+        else:
+            for user in check_db:
+                if first_name == user.first_name and last_name == user.last_name and id_number == user.id_number:
+                    if user.voted:
+                        error = u'ניתן להצביע פעם אחת בלבד'
+                        return render_template('login.html', error=error)
 
-        else: ##validation error
+                    else:
+                        #user = User.query.filter_by(id_number=id_number).first()
+                        login_user(user)  ## built in 'flask login' method that creates a user session
+                        return redirect(url_for('index'))
+
             error = u'המצביע אינו מופיע בבסיס הנתונים'
 
     return render_template('login.html', error=error)
@@ -68,7 +79,9 @@ def logout():
 @app.route('/secret', methods=['GET'])
 @login_required
 def secret():
-    return 'This is a secret page. You are logged in as {} {} {}'.format(current_user.first_name, current_user.last_name, current_user.id_number)
+    return 'This is a secret page. You are logged in as {} {} {}'.format(current_user.first_name,
+                                                                         current_user.last_name,
+                                                                         current_user.id_number)
 
 
 ## will handle the site icon - bonus 2 points for creative new icon
