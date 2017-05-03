5dbf0119bb1b16f04702beb10bf8ea0068504364
Daniega <daniel.gaishuber@gmail.com>
2017-05-03 12:37:41 +0300

    Update and rename cloudShellScript.py to cloudShellScript.sh

--- cloudShellScript.py
+++
@@ -1,4 +0,0 @@
-from cloudshell.api.cloudshell_api import CloudShellAPISession, AppConfiguration
-
-session = CloudShellAPISession('localhost', "admin", "admin", "Global")
-session.ConfigureApps(reservationId='a47ba512-ad70-439e-8375-ec27c0e5c7a2',appConfigurations=[AppConfiguration('Dani-Illya-Daniel Kosher App', None)])
