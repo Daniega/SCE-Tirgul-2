706962398acdb3b4fbe113231e0d226490ba1e3d
illyax <illyax@walla.com>
2017-05-08 11:20:54 +0300

    build

--- app/tests/test_web.py
+++ app/tests/test_web.py
@@ -23,7 +23,7 @@ class test_web(unittest.TestCase):
 
 
     def setUp(self):
-        self.browser = webdriver.Chrome()
+        self.browser = webdriver.Firefox()
         self.browser.get('http://127.0.0.1:5000/')
 
     def tearDown(self):
