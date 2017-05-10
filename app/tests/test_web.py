e8a9dcf5c2e7b24ac89e02690806ab85302bfbb4
illyax <illyax@walla.com>
2017-05-10 10:40:34 +0300

    build

--- app/tests/test_web.py
+++ app/tests/test_web.py
@@ -20,7 +20,6 @@ class test_web(unittest.TestCase):
         assert u'המצביע אינו מופיע בבסיס הנתונים' in self.browser.page_source
 
     def setUp(self):
-        # self.browser = webdriver.Remote("http://192.168.56.1:4444/", desired_capabilities=DesiredCapabilities.CHROME)
         self.browser = webdriver.Firefox()
         self.browser.get("http://192.168.56.1:5000")
 
