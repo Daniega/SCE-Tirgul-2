9ae6b9a59762a3aa6120da87f8c469f6de2639fa
illyax <illyax@walla.com>
2017-05-11 11:46:29 +0300

    build #9324923942394392

--- app/tests/test_web.py
+++ app/tests/test_web.py
@@ -30,7 +30,7 @@ class test_web(LiveServerTestCase):
 
     def populate(self):
         db.session.commit()
-        valid_user = User(320880123, 'illya', 'yurkevich', False)
+        valid_user = User(111, 'some', 'one', False)
         valid_party =  Party(u'העבודה',
                       'https://www.am-1.org.il/wp-content/uploads/2015/03/%D7%94%D7%A2%D7%91%D7%95%D7%93%D7%94.-%D7%A6%D7%99%D7%9C%D7%95%D7%9D-%D7%99%D7%97%D7%A6.jpg')
         db.session.add(valid_party)
@@ -47,9 +47,9 @@ class test_web(LiveServerTestCase):
         self.browser.quit()
 
     def test_login_selenium(self):
-        self.browser.find_element_by_xpath('//*[@id="first_name"]').send_keys('illya')
-        self.browser.find_element_by_xpath('//*[@id="last_name"]').send_keys('yurkevich')
-        self.browser.find_element_by_xpath('//*[@id="id_number"]').send_keys('320880123')
+        self.browser.find_element_by_xpath('//*[@id="first_name"]').send_keys('some')
+        self.browser.find_element_by_xpath('//*[@id="last_name"]').send_keys('one')
+        self.browser.find_element_by_xpath('//*[@id="id_number"]').send_keys('111')
         self.browser.find_element_by_id('EnterBtn').submit()
         assert 'Home' in self.browser.title
 
