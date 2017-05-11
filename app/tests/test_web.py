636618f8b506994fd546f545bb39b734512c84d0
illyax <illyax@walla.com>
2017-05-11 11:39:19 +0300

    build #9324923942394392

--- app/tests/test_web.py
+++ app/tests/test_web.py
@@ -50,7 +50,7 @@ class test_web(LiveServerTestCase):
         self.browser.find_element_by_xpath('//*[@id="first_name"]').send_keys('illya')
         self.browser.find_element_by_xpath('//*[@id="last_name"]').send_keys('yurkevich')
         self.browser.find_element_by_xpath('//*[@id="id_number"]').send_keys('320880123')
-        self.browser.find_element_by_xpath('//*[@id="EnterBtn"]').send_keys(Keys.ENTER)
+        self.browser.find_element_by_xpath('//*[@id="EnterBtn"]').click()
         print self.browser.current_url
         assert 'Home' in self.browser.title
 
