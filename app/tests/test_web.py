bf2c2b6bb8ecb467f1bcce99aad93346a29a246f
illyax <illyax@walla.com>
2017-05-11 12:30:51 +0300

    build #9324923942394392

--- app/tests/test_web.py
+++ app/tests/test_web.py
@@ -29,9 +29,8 @@ class test_web(LiveServerTestCase):
         return self.app
 
     def populate(self):
-        valid_user = User(320880123,'tomer', 'admon')
+        valid_user = User('tomer', 'admon','320880123',False)
         valid_party = Party(u'עלה ירוק', 'static/images/yarok.jpeg', 0)
-        db.session.add(valid_party)
         db.session.add(valid_user)
         db.session.commit()
 
@@ -44,8 +43,7 @@ class test_web(LiveServerTestCase):
         self.browser.quit()
 
     def test_login_selenium(self):
-        self.valid_user = User(320880123,'tomer', 'admon',False)
-        self.valid_party = Party(u'עלה ירוק', 'static/images/yarok.jpeg', 0)
+        self.valid_user = User('tomer', 'admon','320880123',False)
         self.first_name = self.browser.find_element_by_id('first_name')
         self.last_name = self.browser.find_element_by_id('last_name')
         self.id_num = self.browser.find_element_by_id('id_number')
