4cabdc0952d48d1ba89f3663511bc5a2ed7d740c
illyax <illyax@walla.com>
2017-05-11 11:18:08 +0300

    build #9324923942394392

--- app/tests/test_web.py
+++ app/tests/test_web.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 import os
 import unittest
 
@@ -9,15 +8,18 @@ from selenium import webdriver
 from app import app, db
 from app.models import User, Party
 
+
 basedir = os.path.abspath(os.path.dirname(__file__))
-class AppTestCase(LiveServerTestCase):
+
+class test_web(unittest.TestCase):
     def create_app(self):
         self.app = app
         self.app.config['TESTING'] = True
         self.app.config['WTF_CSRF_ENABLED'] = False
         self.app.config['LIVESERVER_PORT'] = 8943
         self.app.config['WTF_CSRF_ENABLED'] = False
-        self.app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'test.db') #'sqlite:///:memory:'
+        self.app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,
+                                                                                 'test.db')  # 'sqlite:///:memory:'
         db.init_app(self.app)
         with self.app.app_context():  # app context
             db.drop_all()
@@ -36,38 +38,29 @@ class AppTestCase(LiveServerTestCase):
 
     def setUp(self):
         """Setup the test driver and create test users"""
-        self.driver = webdriver.PhantomJS()
-        self.driver.get(self.get_server_url())
+        self.browser = webdriver.PhantomJS()
+        self.browser.get(self.get_server_url())
 
     def tearDown(self):
-        self.driver.quit()
+        self.browser.quit()
 
-    def test_valid_user_selenium(self):
-        self.valid_user = User(111111, 'firstName', 'lastName', False)
-        self.valid_party = Party(u'עלה ירוק', 'static/images/yarok.jpeg', 0)
-        self.first_name = self.driver.find_element_by_id('first_name')
-        self.last_name = self.driver.find_element_by_id('last_name')
-        self.id_num = self.driver.find_element_by_id('id_num')
-        self.login_button = self.driver.find_element_by_id('login_button')
-        self.first_name.send_keys(self.valid_user.first_name)
-        self.last_name.send_keys(self.valid_user.last_name)
-        self.id_num.send_keys(self.valid_user.id)
-        self.login_button.submit()
-        print(self.driver.title)
-        assert 'Home' in self.driver.title
+    def test_login_selenium(self):
+        self.browser.find_element_by_xpath('//*[@id="first_name"]').send_keys('illya')
+        self.browser.find_element_by_xpath('//*[@id="last_name"]').send_keys('yurkevich')
+        self.browser.find_element_by_xpath('//*[@id="id_number"]').send_keys('320880123')
+        self.browser.find_element_by_xpath('//*[@id="EnterBtn"]').send_keys(Keys.ENTER)
+        print self.browser.current_url
+        self.assertEqual('http://127.0.0.1:5000/login?next=%2F', self.browser.current_url)
 
-    def test_invalid_user_selenium(self):
-        self.first_name = self.driver.find_element_by_id('first_name')
-        self.last_name = self.driver.find_element_by_id('last_name')
-        self.id_num = self.driver.find_element_by_id('id_num')
-        self.login_button = self.driver.find_element_by_id('login_button')
-        self.first_name.send_keys('invalidFirstName')
-        self.last_name.send_keys('invalidLastName')
-        self.id_num.send_keys('invalidID')
-        self.login_button.submit()
-        print(self.driver.title)
-        assert 'Home' not in self.driver.title
+    def test_noSuchUser_selenium(self):
+        self.browser.find_element_by_xpath('//*[@id="first_name"]').send_keys('no')
+        self.browser.find_element_by_xpath('//*[@id="last_name"]').send_keys('such')
+        self.browser.find_element_by_xpath('//*[@id="id_number"]').send_keys('user')
+        self.browser.find_element_by_xpath('//*[@id="EnterBtn"]').click()
+        assert "Flask Intro - login page" in self.browser.title
 
 
-if (__name__ == '__main__'):
+
+
+if __name__ == "__main__":
     unittest.main()
