f7a240463868b4b4722b6e86bc870792c5ede105
illyax <illyax@walla.com>
2017-05-11 11:54:04 +0300

    build #9324923942394392

--- app/tests/test_web.py
+++ app/tests/test_web.py
@@ -51,6 +51,7 @@ class test_web(LiveServerTestCase):
         self.browser.find_element_by_xpath('//*[@id="last_name"]').send_keys('one')
         self.browser.find_element_by_xpath('//*[@id="id_number"]').send_keys('111')
         self.browser.find_element_by_id('EnterBtn').submit()
+        self.browser.implicitly_wait(5)
         print self.get_server_url()
         assert 'Home' in self.browser.title
 
