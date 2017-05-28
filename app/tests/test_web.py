b84b0bc657177cbdf2218636ef0a0d62dd0abb24
illyax <illyax@walla.com>
2017-05-28 14:43:37 +0300

    build #9324923942394392

--- app/tests/test_web.py
+++ app/tests/test_web.py
@@ -72,7 +72,7 @@ class test_web(LiveServerTestCase):
         self.last_name.send_keys(self.valid_user.last_name)
         self.id_num.send_keys('320880123')
         self.login_button.submit()
-        self.browser.implicitly_wait(10)
+        self.browser.implicitly_wait(2)
         print ('here '+self.browser.title)
         self.browser.find_element_by_id('הליכוד'.encode('utf-8')).submit()
         self.browser.find_element_by_id('btnSubmit').submit()
