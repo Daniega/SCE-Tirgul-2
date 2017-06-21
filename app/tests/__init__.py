d681ee45a95397397a1d2fe510a2e885819b4cb3
illyax <illyax@walla.com>
2017-06-21 12:13:10 +0300

    build #9324923942394392

--- app/tests/__init__.py
+++ app/tests/__init__.py
@@ -1,13 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from db_create import db_create
 
 app = Flask(__name__)
 app.config.from_object('test_config')
 app.app_context().push()
 test_db = SQLAlchemy(app)
-db_create(test_db)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
